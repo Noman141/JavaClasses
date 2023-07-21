38cbb6960b0840db68984714d56482c67b2b2781
Noman141 <nomanjahid203@gmail.com>
2023-07-21 13:04:03 +0600

    Submit Third Assignment

---
+++ src/Assignment3/IfElseIfladder.java
@@ -0,0 +1,14 @@
+package Assignment3;
+
+public class IfElseIfladder {
+    public static void main(String[] args) {
+        int time=9;
+        if(time < 10){
+            System.out.println("Good morning");
+        }else if(time < 18){
+            System.out.println("Good noon");
+        }else {
+            System.out.println("Good evening");
+        }
+    }
+}
---
+++ src/Assignment3/JavaForEachloop.java
@@ -0,0 +1,15 @@
+package Assignment3;
+
+public class JavaForEachloop {
+    public static void main(String[] args) {
+        int[] time={9,8,10,12};
+        for (int t: time) {
+            System.out.println(t);
+        }
+
+        String[] Name={"Jahid","Noman","Hasan","Tasfia","Tabassum"};
+        for (String t: Name) {
+            System.out.println(t);
+        }
+    }
+}
