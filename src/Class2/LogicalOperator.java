8ad97a0c2521e6159707e77460167602faa0c3b0
Noman141 <nomanjahid203@gmail.com>
2023-07-21 13:09:45 +0600

    Submit Second Assignment

---
+++ src/Class2/LogicalOperator.java
@@ -0,0 +1,10 @@
+package Class2;
+
+public class LogicalOperator {
+    public static void main(String[] args) {
+        int x=20;
+        int y=10;
+        System.out.println(x>y || x==y);
+        System.out.println(x>y && x==y);
+    }
+}
